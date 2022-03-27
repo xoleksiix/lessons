3e586a67d0035ccc654f0f696d7826058fd45c34
xoleksiix <19oleksii96@gmail.com>
2022-03-27 16:07:24 +0300

    add 8

--- homework8.py
+++ homework8.py
@@ -1,11 +1,17 @@
-xlst = [150, 193, 180, 170, 175]
-x = int(input())
+import random
+
+xlst = [random.randint(120, 200) for _ in range(10)]
+x = int(input("Рост Пети:"))
 
 xlst.sort(reverse=True)
+print(xlst)
 
 for i in range(len(xlst)):
     if x == xlst[i]:
-        print(f"Петя по порядку #{i + 2}")
+        for j in range(i + 1, len(xlst)):
+            if xlst[i] != xlst[j]:
+                print(f"Петя по порядку #{j + 1}")
+                break
         break
     elif x > xlst[i]:
         print(f"Петя по порядку #{i + 1}")
