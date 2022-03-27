33132e292dd4de78d64880b1360be08cb9031740
xoleksiix <19oleksii96@gmail.com>
2022-03-27 16:57:35 +0300

    add 8

--- homework8.py
+++ homework8.py
@@ -1,13 +1,11 @@
 import random
 
-xlst = [random.randint(120, 200) for _ in range(10)]
 x = int(input("Рост Пети:"))
-
+xlst = [random.randint(120, 200) for _ in range(22)]
 xlst.sort(reverse=True)
-print(xlst)
 
 for i in range(len(xlst)):
-    if x == xlst[i]:
+    if x == xlst[i]:        #есть ребята с таким же ростом как у Пети
         for j in range(i + 1, len(xlst)):
             if xlst[i] != xlst[j]:
                 print(f"Петя по порядку #{j + 1}")
@@ -15,4 +13,7 @@ for i in range(len(xlst)):
         break
     elif x > xlst[i]:
         print(f"Петя по порядку #{i + 1}")
+        break
+    else:                   #если Петя ниже всех
+        print(f"Петя по порядку #{len(xlst) + 1}")
         break
