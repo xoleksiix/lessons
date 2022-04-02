08f15199ddb0978e3deaffbdc959b69b91cadbd2
xoleksiix <19oleksii96@gmail.com>
2022-04-02 19:12:38 +0300

    add 14

--- homework14.py
+++ homework14.py
@@ -1,9 +1,9 @@
 def multiplication_of_numbers(x):
     result = 1
     for i in str(x):
-        result = int(i) * result if i != "0" else result
+        result = int(i) * result if int(i) != 0 else result
     return result
 
 if __name__ == '__main__':
     numbers = int(input("Введите число:"))
-    print(multiplication_of_numbers(numbers))
+    print(f"Результат: {multiplication_of_numbers(numbers)}.")
