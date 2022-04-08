c2c76a0654acdf0497715851f83d32993a4cd678
xoleksiix <19oleksii96@gmail.com>
2022-04-08 20:42:50 +0300

    working  hm15 comments

--- homework15.py
+++ homework15.py
@@ -1,7 +1,13 @@
 from functools import wraps
 
 
-def authenticate(check: bool, i) -> bool:
+def authenticate(check: bool, i: int) -> bool:
+    """
+
+    :param check: bool
+    :param i: int
+    :return: bool
+    """
     if check:
         print("Вы в системе!")
         return True
@@ -12,25 +18,35 @@ def authenticate(check: bool, i) -> bool:
 
 
 def check_password(data: dict, username: str, password: str) -> bool:
+    """
+    It accepts a dictionary of usernames and their passwords,
+    and username and password that the user entered as strings.
+    Checks if the user is in the dictionary (key) and
+    if the password matches (value).
+    Returns TRUE if the username is in the dictionary and the password matches,
+    otherwise FALSE.
+    :param data: dict
+    :param username: str
+    :param password: str
+    :return: bool
+    """
     return True if (username in data) and (data.get(username) == password) \
         else False
 
 
-def login_wrapper_with_args(*args, **kwargs):
-    print(args, kwargs)
-
+def login_wrapper_with_args(*args, **_kwargs):
     def login_wrapper(func):
         @wraps(func)
         def wrapper():
-            for i in reversed(range(3)):
-                func()
+            for i in reversed(range(3)):    # loop to check the number
+                func()                      # of login attempts
                 usernames_and_passwords = args[0]
                 check = check_password(usernames_and_passwords,
                                        username,
                                        password)
                 result = authenticate(check, i)
-                if result == 1:
-                    break
+                if result == 1:             # in case of a successful attempt
+                    break                   # break
             return result
 
         return wrapper
@@ -38,13 +54,19 @@ def login_wrapper_with_args(*args, **kwargs):
     return login_wrapper
 
 
-data = {"qwerty": "qwerty",
-        "second": "123456",
-        "oldschol": "0000"}
+data = {"qwerty": "qwerty",     # не получилось вызвать словарь
+        "lol": "123456789",     # не объявив его перед декоратором
+        "admin": "admin"}
 
 
 @login_wrapper_with_args(data)
 def login() -> bool:
+    """
+    Receives input data through user input and declares in global variables,
+    returns a boolean value in case of successful data input
+    (always returns TRUE if there is no username and password verification).
+    :return: bool
+    """
     global username, password
     username = input("Name:")
     password = input("Password:")
