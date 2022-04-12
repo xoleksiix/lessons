401e9ba4a013dbb82ed37b33a494b5fe7671bd5f
xoleksiix <19oleksii96@gmail.com>
2022-04-12 21:18:19 +0300

    add hm16

--- homework16.py
+++ homework16.py
@@ -1,17 +1,21 @@
-def check_win(board):
-    if (board[0][0] == board[0][1] == board[0][2]) \
-            or (board[0][0] == board[1][1] == board[0][2]) \
+def check_win(board: list[str]) -> str:
+    if (board[0][1] == board[1][1] == board[2][2]) \
+            or (board[0][2] == board[1][1] == board[2][0]) \
+            or (board[0][1] == board[1][1] == board[2][1]) \
+            or (board[1][0] == board[1][1] == board[1][2]):
+        return board[1][1]
+        #комбинации через центральную клетку
+    elif (board[0][0] == board[0][1] == board[0][2]) \
             or (board[0][0] == board[1][0] == board[2][0]):
         return board[0][0]
-    elif (board[2][2] == board[2][1] == board[2][0]) \
-            or (board[2][2] == board[1][1] == board[0][0]) \
-            or (board[2][2] == board[1][2] == board[0][2]):
+        #через левый верхний угол, искллючая диагональ
+    elif board[2][0] == board[2][1] == board[2][2] \
+            or board[0][2] == board[1][2] == board[2][2]:
         return board[2][2]
-    elif board[1][0] == board[1][1] == board[1][2] \
-            or board[0][1] == board[1][1] == board[2][1]:
-        return board[1][1]
+        #через нижний правый угол, исключая диагональ
     else:
         return "D"
+        #нет победителя
 
 
 if __name__ == '__main__':
@@ -20,4 +24,4 @@ if __name__ == '__main__':
              "XOX",
              "XOO"]
 
-    print(check_win(board))
+  print(check_win(board))
